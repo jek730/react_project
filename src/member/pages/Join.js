a456099141b456230dd0dfcf9a3d378fbf22c804
jek730 <jejek730@gmail.com>
2024-04-15 17:56:17 +0900

    Revert "회우너 가입 페이지 구상 중"
    This reverts commit 7c673c034a7d7c9cfc2db215fe13e5328f96c589.

--- src/member/pages/Join.js
+++ src/member/pages/Join.js
@@ -1,23 +1,7 @@
 import React from 'react';
-import { Helmet } from 'react-helmet-async';
-import { useTranslation } from 'react-i18next';
-import JoinForm from '../components/JoinForm';
-import { OuterBox } from '../../commons/components/LayoutBox';
 
 const Join = () => {
-  const { t } = useTranslation();
-
-  return (
-    <>
-      <Helmet>
-        <title>{t('회원가입')}</title>
-      </Helmet>
-      <OuterBox>
-        <h1>{t('회원가입')}</h1>
-        <JoinForm />
-      </OuterBox>
-    </>
-  );
+  return <h1>회원 가입페이지</h1>;
 };
 
 export default React.memo(Join);
